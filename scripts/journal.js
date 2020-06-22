faf8fa34c2ac251ce76e78aef74f9547ba2874d3
chrismccolgan <mccolganchrisj@gmail.com>
2020-06-22 15:57:51 -0500

    got the edit button working!

--- scripts/journal.js
+++ scripts/journal.js
@@ -2,6 +2,20 @@ import API from "./data.js"
 import renderJournalEntries from "./entryList.js"
 import createEntryObject from "./createEntry.js"
 
+const updateFormFields = (journalObj) => {
+    const hiddenEntryID = document.querySelector("#entryID");
+    const journalDateInput = document.querySelector("#journalDate");
+    const journalConceptsInput = document.querySelector("#journalConcepts");
+    const journalMoodInput = document.querySelector("#journalMood");
+    const journalEntryInput = document.querySelector("#journalEntry");
+
+    hiddenEntryID.value = journalObj.id;
+    journalDateInput.value = journalObj.date
+    journalConceptsInput.value = journalObj.concepts;
+    journalMoodInput.value = journalObj.mood;
+    journalEntryInput.value = journalObj.entry;
+}
+
 const showEntries = () => {
     document.querySelector(".entryLog").innerHTML = ""
     API.getJournalEntries()
@@ -10,26 +24,34 @@ const showEntries = () => {
 
 // This makes the save button work
 document.querySelector("#saveButton").addEventListener("click", clickEvent => {
+    const IDVal = document.querySelector("#entryID").value
     const dateVal = document.querySelector("#journalDate").value
     const conceptVal = document.querySelector("#journalConcepts").value
     const entryVal = document.querySelector("#journalEntry").value
     const moodVal = document.querySelector("#journalMood").value
+    const newJournalObject = createEntryObject(dateVal, conceptVal, entryVal, moodVal)
 
     if (dateVal === "" || conceptVal === "" || entryVal === "" || moodVal === "") {
         alert("Please complete all fields")
+    } else if (IDVal !== "") {
+        API.editJournalEntry(IDVal, newJournalObject)
+            .then(showEntries)
     } else {
-        const newJournalObject = createEntryObject(dateVal, conceptVal, entryVal, moodVal)
         API.saveJournalEntry(newJournalObject)
             .then(showEntries)
     }
 })
 
-// This makes the delete button work
+// This makes the delete and edit button work
 document.querySelector(".entryLog").addEventListener("click", event => {
     if (event.target.id.startsWith("deleteJournalEntry--")) {
         const entryToDelete = event.target.id.split("--")[1]
         API.deleteJournalEntry(entryToDelete)
             .then(showEntries)
+    } else if (event.target.id.startsWith("editJournalEntry--")) {
+        const entryToEdit = event.target.id.split("--")[1]
+        API.getSingleEntry(entryToEdit)
+            .then(entryToEdit => updateFormFields(entryToEdit))
     }
 })
 
